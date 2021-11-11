3013d8132dad46eb38d1c7f08c3dccdb2aebbc14
hanh <huitan.han@bioduro-sundia.com>
2021-11-11 13:23:43 +0800

    init

---
+++ src/views/BlogEdit.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <Header></Header>
+        <div class="m-content">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="标题" prop="title">
+                    <el-input v-model="ruleForm.title"></el-input>
+                </el-form-item>
+
+                <el-form-item label="摘要" prop="description">
+                    <el-input type="textarea" v-model="ruleForm.description"></el-input>
+                </el-form-item>
+                <el-form-item label="内容" prop="content">
+                    <mavon-editor v-model="ruleForm.content"/>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from "../components/Header";
+    export default {
+        name: "BlogEdit",
+        components: {Header},
+        data() {
+            return {
+                ruleForm: {
+                    title: '',
+                    description: '',
+                    content: ''
+                },
+                rules: {
+                    title: [
+                        { required: true, message: '请输入标题', trigger: 'blur' },
+                        { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                    ],
+                    description: [
+                        { required: true, message: '请选择摘要', trigger: 'blur' }
+                    ],
+                    content: [
+                        { message: '请输入内容', trigger: 'blur' }
+                    ]
+                }
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        const _this = this
+                        _this.$axios.post("/blog/edit",this.ruleForm,{
+                            headers: {
+                                "Authorization": localStorage.getItem("token")
+                            }
+                        }).then(res =>{
+                            console.log(res)
+                        })
+
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+.m-content {
+  text-align: center;
+}
+</style>
